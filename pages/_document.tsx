e357f52e297138b7e08678f1a3c972d072090cca
omsrg <siregar.om30@gmail.com>
2022-02-12 06:47:59 +0700

    added file _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						rel='preload'
+						href='/fonts/inter-var-latin.woff2'
+						as='font'
+						type='font/woff2'
+						crossOrigin='anonymous'
+					/>
+					<link
+						rel='apple-touch-icon'
+						sizes='180x180'
+						href='/favicon/apple-touch-icon.png'
+					/>
+					<link
+						rel='icon'
+						type='image/png'
+						sizes='32x32'
+						href='/favicon/favicon-32x32.png'
+					/>
+					<link
+						rel='icon'
+						type='image/png'
+						sizes='16x16'
+						href='/favicon/favicon-16x16.png'
+					/>
+					<link rel='manifest' href='/favicon/site.webmanifest' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
